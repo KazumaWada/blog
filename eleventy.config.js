8010e94b6aa25dedee52d57d610238eb8b623939
KazumaWada <kazumawadaa@gmail.com>
2025-03-28 18:26:16 +0900

    add note.com on nav

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,27 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import markdownIt from "markdown-it";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 
 export default async function(eleventyConfig) {
  eleventyConfig.addPlugin(syntaxHighlight);
-
+	
  let options = {
 		breaks: true,
 		linkify: true,
 	};
  eleventyConfig.setLibrary("md", markdownIt(options));
+
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin,{
+		formats: ["png"],
+		// widths: ["60px"],
+		htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			},
+			pictureAttributes: {}
+		},
+	});
 
 };
