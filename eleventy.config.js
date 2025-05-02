2e34019b9b94b2d90e0df97b4cd152e2ac4355d8
KazumaWada <kazumawadaa@gmail.com>
2025-05-02 19:00:43 +0900

    commit

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,27 +1,27 @@
-import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
-import markdownIt from "markdown-it";
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+// import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+// import markdownIt from "markdown-it";
+// import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 
-export default async function(eleventyConfig) {
- eleventyConfig.addPlugin(syntaxHighlight);
+// export default async function(eleventyConfig) {
+//  eleventyConfig.addPlugin(syntaxHighlight);
 	
- let options = {
-		breaks: true,
-		linkify: true,
-	};
- eleventyConfig.setLibrary("md", markdownIt(options));
+//  let options = {
+// 		breaks: true,
+// 		linkify: true,
+// 	};
+//  eleventyConfig.setLibrary("md", markdownIt(options));
 
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin,{
-		formats: ["png"],
-		// widths: ["60px"],
-		htmlOptions: {
-			imgAttributes: {
-				loading: "lazy",
-				decoding: "async",
-			},
-			pictureAttributes: {}
-		},
-	});
+// 	eleventyConfig.addPlugin(eleventyImageTransformPlugin,{
+// 		formats: ["png"],
+// 		htmlOptions: {
+// 			imgAttributes: {
+// 				loading: "lazy",
+// 				decoding: "async",
+// 			},
+// 			pictureAttributes: {}
+// 		},
+// 	}
+// );
 
-};
+// };
